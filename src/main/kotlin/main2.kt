de4bf2d1c13da3a2ea6e49e7c46c9659b2a5ae5f
Mikhail Lantsetov <lansm77@gmail.com>
2020-05-16 19:59:47 +0300

    Initial commit

---
+++ src/main/kotlin/main2.kt
@@ -0,0 +1,14 @@
+class Person(val name: String, val age: Int){//первичный конструктор
+    fun sayHello(){
+        println("Hello")
+    }
+    fun getInfo() : String{
+        return "Name: $name  Age: $age"
+    }
+}
+
+fun main(args: Array<String>) {
+    val bob: Person = Person("Bob", 23) //создание объекта
+    bob.sayHello()          // Hello
+    println(bob.getInfo())  // Name: Bob  Age: 23
+}

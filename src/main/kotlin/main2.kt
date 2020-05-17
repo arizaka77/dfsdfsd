959a7830508bbffb587aedb88bd45e52b59c248b
Ilya Poroshin <kasyabasya@mail.ru>
2020-05-17 16:50:21 +0300

    added spaces in comments

--- src/main/kotlin/main2.kt
+++ src/main/kotlin/main2.kt
@@ -6,18 +6,19 @@ class Person(val name: String, val age: Int){//первичный констру
         return "Name: $name  Age: $age"
     }
 }
-
+// Skrill1945 исправил комменты (добавил пробельчики)))
 fun main(args: Array<String>) {
-    val alice: Person = Person("Bob", 23) //создание объекта
+    val alice: Person = Person("Bob", 23) // создание объекта
     alice.sayHello()          // Hello
     println(alice.getInfo())  // Name: Bob  Age: 23
-    //внёс изменения
+    // внёс изменения
     val bob: Person = Person("Alice", 32)
     println(bob.getInfo())
-    //нужно сделать commit
-    //commit -- сохранить изменения(пока локально!!!) ГО СДЕЛАЕМ
-    //----------------
-    //Создаем новую ветку(branch) checkout - переключение (между ветками)
-    //Создал ветку
-    //---В новой ветке внес изменения
+    // нужно сделать commit
+    // commit -- сохранить изменения(пока локально!!!) ГО СДЕЛАЕМ
+    // ----------------
+    // Создаем новую ветку(branch) checkout - переключение (между ветками)
+    // Создал ветку
+    // ---В новой ветке внес изменения
+
 }
